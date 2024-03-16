f1f3321d53a88674915c3ddfd7c5e8a02e8a4c7b
UICHEOL-HWANG <icuchoel@gmail.com>
2024-03-17 01:30:47 +0900

    Feat: helm chart 추가

--- deploy/api/main.py
+++ deploy/api/main.py
@@ -1,7 +1,17 @@
-import pandas as pd 
-from fastapi import FastAPI
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from fastapi.middleware.cors import CORSMiddleware
+import mlflow
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import mlflow.pytorch
+import torch
+from PIL import Image
+import io
+import numpy as np
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://34.64.144.27:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://34.64.119.81:5000"
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
 
 
 # Create a FastAPI instance
@@ -15,6 +25,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+model = mlflow.pytorch.load_model("runs:/2fd50ae92cbd43efbe305a1d7f457596/model")
+model.eval()
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert('L')
+    image = image.resize((28, 28))
+    image = np.array(image)
+    image = torch.tensor(image).float().unsqueeze(0).unsqueeze(0) / 255.0
+    prediction = model(image).argmax(1).item()
+    return JSONResponse(content={"prediction": prediction})

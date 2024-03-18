07f31579ba90f0b6fa89aa98757ad9eebe45bc42
UICHEOL_HWANG <icuchoel@gmail.com>
2024-03-18 13:56:22 +0900

    Fix: 차트결과 재생성

--- deploy/api/main.py
+++ deploy/api/main.py
@@ -30,14 +30,6 @@ app.add_middleware(
 model = mlflow.pytorch.load_model("runs:/508bfe3947f6400f8fd6d6b68634b33c/model")
 model.eval()
 
-def create_chart(prediction):
-    # 추론 결과를 차트로 만듭니다. 여기서는 예시로 단순한 바 차트를 사용합니다.
-    fig, ax = plt.subplots()
-    ax.bar(["Predicted Number"], [prediction])
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    return buffer
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -45,12 +37,22 @@ async def predict(file: UploadFile = File(...)):
     image = Image.open(io.BytesIO(contents)).convert('L')
     image = image.resize((28, 28))
     image_np = np.array(image)
-    tensor = torch.tensor(image_np).float().unsqueeze(0).unsqueeze(0) / 255.0
+    image = torch.tensor(image_np).float().unsqueeze(0).unsqueeze(0) / 255.0
     
-    prediction = model(tensor).argmax(1).item()
+    # 모델로부터 예측 결과를 얻습니다.
+    with torch.no_grad():
+        prediction = model(image).softmax(1).numpy()
+
+    # 예측 결과를 시각화합니다.
+    fig, ax = plt.subplots()
+    ax.imshow(image_np, cmap='gray', interpolation='none')
+    ax.set_title(f'Predict Number is {np.argmax(prediction)}')
+    # 차트를 이미지 버퍼로 변환합니다.
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    plt.close(fig)
+    buffer.seek(0)
+    img_str = base64.b64encode(buffer.getvalue()).decode()
     
-    # 추론 결과를 차트로 변환
-    chart_buffer = create_chart(prediction)
-    img_str = base64.b64encode(chart_buffer.getvalue()).decode()
-    
-    return JSONResponse(content={"prediction": prediction, "chart": img_str})
+    # 예측 결과를 JSON으로 반환합니다.
+    return JSONResponse(content={"prediction": np.argmax(prediction), "chart": img_str})
